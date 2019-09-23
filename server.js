415d96f25286045a54970e6ab3c9ee6cbff10af0
quan005 <ralphealfreeman@gmail.com>
2019-09-23 16:06:09 -0700

    fixed postgresql bug

--- server.js
+++ server.js
@@ -15,4 +15,13 @@ server.use(cors());
 server.use('/api/users', AuthRouter)
 server.use('/api/users', UserRouter);
 
+server.get('/', (req, res) => {
+    res.status(200).send(`
+      <div>
+        <h2>Server Running Live...</h2>
+        <p>Url to Login endpoint <strong>'/api/login'</strong></p>
+        <p>Url to Register endpoint <strong>'/api/register'</strong></p>
+      </div>`);
+  });
+
 module.exports = server;
