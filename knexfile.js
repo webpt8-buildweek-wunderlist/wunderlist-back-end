0c52b0ad017e5186932cd6f59aa077c106197a72
quan005 <ralphealfreeman@gmail.com>
2019-09-23 12:59:34 -0700

    gitignore fix

--- knexfile.js
+++ knexfile.js
@@ -2,42 +2,42 @@ const dbConnection = process.env.DATABASE_URL
 
 module.exports = {
 
-    development: {
-      client: 'sqlite3',
-      useNullAsDefault: true,
-      connection: {
-        filename: './data/users.db3',
-      },
-      migrations: {
-        directory: './data/migrations'
-      },
-      seeds: {
-        directory: './data/seeds'
-      },
-      pool: {
-        afterCreate: (conn, done) => {
-          conn.run('PRAGMA foreign_keys = ON', done);
-        },
-      },
-    },
-    testing: {
-      client: 'sqlite3',
-      useNullAsDefault: true,
-      connection: {
-        filename: './data/test.db3',
-      },
-      migrations: {
-        directory: './data/migrations'
-      },
-      seeds: {
-        directory: './data/seeds'
-      },
-      pool: {
-        afterCreate: (conn, done) => {
-          conn.run('PRAGMA foreign_keys = ON', done);
-        },
-      },
-    },
+    // development: {
+    //   client: 'sqlite3',
+    //   useNullAsDefault: true,
+    //   connection: {
+    //     filename: './data/users.db3',
+    //   },
+    //   migrations: {
+    //     directory: './data/migrations'
+    //   },
+    //   seeds: {
+    //     directory: './data/seeds'
+    //   },
+    //   pool: {
+    //     afterCreate: (conn, done) => {
+    //       conn.run('PRAGMA foreign_keys = ON', done);
+    //     },
+    //   },
+    // },
+    // testing: {
+    //   client: 'sqlite3',
+    //   useNullAsDefault: true,
+    //   connection: {
+    //     filename: './data/test.db3',
+    //   },
+    //   migrations: {
+    //     directory: './data/migrations'
+    //   },
+    //   seeds: {
+    //     directory: './data/seeds'
+    //   },
+    //   pool: {
+    //     afterCreate: (conn, done) => {
+    //       conn.run('PRAGMA foreign_keys = ON', done);
+    //     },
+    //   },
+    // },
     production: {
       client: 'pg',
       connection: dbConnection,
