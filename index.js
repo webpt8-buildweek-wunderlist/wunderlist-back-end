9e2d8ce7fa5456db4177d73c588cc050a732c564
quan005 <ralphealfreeman@gmail.com>
2019-09-23 16:03:11 -0700

    fixed postgresql bug

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require('./server.js');
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 server.listen(port, () => {
   console.log(`Listening on port ${port}...`);
